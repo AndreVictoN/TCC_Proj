c034bf04322a8f8bff75a97cf5072737678e835c
André Victor Gonçalves Nascimento <andrevictor20011@gmail.com>
2025-09-23 16:09:09 -0300

    First cutscene lines added

--- Assets/Scripts/Managers/CutsceneManager.cs
+++ Assets/Scripts/Managers/CutsceneManager.cs
@@ -1,16 +1,104 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Core.Singleton;
+using UnityEngine.UI;
+using TMPro;
 
 public class CutsceneManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Texts")]
+    public GameObject dialoguePanel;
+    public TextMeshProUGUI dialogueText;
+    public List<string> dialogue = new List<string>();
+    public float wordSpeed = 0.6f;
+
+    private bool _isTyping;
+    private bool _skipped;
+    private bool _canSkip;
+    private int _i;
+
+    protected IEnumerator Typing()
     {
-        
+        _isTyping = true;
+
+        if (dialogueText.text != "")
+        {
+            dialogueText.text = "";
+        }
+
+        foreach (char letter in dialogue[_i].ToCharArray())
+        {
+            if (dialogueText.text != dialogue[_i])
+            {
+                dialogueText.text += letter;
+                yield return new WaitForSeconds(wordSpeed);
+            }
+        }
+
+        _isTyping = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    public virtual void NextLine()
     {
-        
+
+        if(_i < dialogue.Count - 1)
+        {
+            _i++;
+            dialogueText.text = "";
+            StartCoroutine(Typing());
+        }else
+        {
+            _i = 0;
+            if(dialoguePanel != null)
+            {
+                dialoguePanel.SetActive(false);
+            }
+        }
+    }
+
+    protected IEnumerator StartAutomaticTalk(EventsEnum evt)
+    {
+        GameObject skipText = null;
+
+        if (!dialoguePanel.activeSelf)
+        {
+            dialogueText.text = "";
+            dialogueText.alignment = TextAlignmentOptions.Center;
+            dialogueText.fontStyle = FontStyles.Italic;
+            dialoguePanel.SetActive(true);
+            _i = 0;
+
+            GameObject npcImage = GameObject.FindGameObjectWithTag("NPC_Image");
+            npcImage.GetComponent<Image>().color = new Vector4(0, 0, 0, 0);
+
+            GameObject npcName = GameObject.FindGameObjectWithTag("NPC_Name");
+            npcName.GetComponent<TextMeshProUGUI>().text = "";
+
+            GameObject playerImage = GameObject.FindGameObjectWithTag("Player_Image");
+            playerImage.GetComponent<Image>().color = new Vector4(playerImage.GetComponent<Image>().color.r, playerImage.GetComponent<Image>().color.g, playerImage.GetComponent<Image>().color.b, 1f);
+
+            skipText = GameObject.FindGameObjectWithTag("SkipText");
+            skipText.SetActive(false);
+            StartCoroutine(Typing());
+        }
+
+        while (_i != dialogue.Count - 1)
+        {
+            yield return null;
+
+            if (!_isTyping)
+            {
+                yield return new WaitForSeconds(1f);
+                NextLine();
+            }
+        }
+
+        if (skipText != null) skipText.SetActive(true);
+
+        _canSkip = true;
+        _skipped = false;
     }
 }
